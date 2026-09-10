546cc37035b360ab6fdf152802e1a350a8ed04de
abdullah mohammed <abdullahmotwa@gmail.com>
2026-09-10 11:21:46 +0300

    completing yesterday's js lab

--- js/implement-a-matching-object-filter.js
+++ js/implement-a-matching-object-filter.js
@@ -1,18 +1,9 @@
 function whatIsInAName (arr, obj){
-const key = Object.keys(obj)
-const value= Object.values(obj)
-
-let match = [];
-for (let i=0; i<arr.length; i++){
-  for (let x of key){
-    console.log(x)
-    for (let y of value){
-  if (arr[i][x]=== y){
-    console.log(x)
-match.push(arr[i])
-  }}
-}}
+let value = JSON.stringify(obj).replaceAll(/[{}]/g, "").split(",")
+console.log(value)
+let match = arr.filter((ob) => value.every((n) => JSON.stringify(ob).includes(n)))
 return match;
 }
 
-console.log(whatIsInAName([{ "apple": 1, "bat": 2 }, { "bat": 2 }, { "apple": 1, "bat": 2, "cookie": 2 }], { "apple": 1, "bat": 2 }))
+
+
